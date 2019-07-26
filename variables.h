c1e48575014e4044ba3ff3bccbf209cd8d86cb35
weagarciaro <53313154+weagarciaro@users.noreply.github.com>
2019-07-26 01:36:06 -0500

    Add files via upload

---
+++ variables.h
@@ -0,0 +1,17 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+
+
+
+//Timer
+void wait_ms(unsigned int time);
+void wait_us(unsigned int time);
+
+
+//Aplicacion
+unsigned int dataA;
+unsigned int state_player;
+
+
+#endif
